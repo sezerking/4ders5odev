22a661c39167007fb11cf0a14cd5411a4c99b3a3
sezer karakaş <sezeroooo@hotmail.com>
2021-01-23 12:29:23 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> MyDicitoanry = new Dictionary<int,string>();
+            MyDicitoanry.Add(0, "Kar kuresi");
+            MyDicitoanry.Add(1, "Parfüm");
+            MyDicitoanry.Add(2, "Oyuncak ayı");
+
+            Console.WriteLine(MyDicitoanry[0]);
+            Console.WriteLine(MyDicitoanry[1]);
+            Console.WriteLine(MyDicitoanry[2]);
+
+        }
+    }
+}
